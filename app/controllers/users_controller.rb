f2fb42c9556d1b55f89d2c2901d9095df8fcac8d
edgar ludena <edgar.ludena90@gmail.com>
2023-01-24 17:17:01 -0500

    updated seeds and migrations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,6 +46,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:name, :username, :email, :password_digest, :burger_id)
+      params.require(:user).permit(:name, :username, :email, :password_digest)
     end
 end
--- app/models/burger.rb
+++
@@ -1,2 +0,0 @@
-class Burger < ApplicationRecord
-end
