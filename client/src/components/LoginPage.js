c2ee57506c0c0e50308a03f024b49e8d7f1bd6ba
Nick Landas <nick.landas@gmail.com>
2023-01-24 10:53:13 -0600

    Added component for signup and login and added user controller and model logic.

---
+++ client/src/components/LoginPage.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+
+const LoginPage = ({updateUser}) => {
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const [formData, setFormData] = useState({
+        username: '',
+        password: ''
+    })
+    const [errors, setErrors] = useState([])
+    const { username, password } = formData
+
+    function submitHandler(e) {
+        e.preventDefault()
+        const user = {
+            username,
+            password
+        }
+
+        fetch(`/login`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(user)
+        })
+            .then(res => {
+                if (res.ok) {
+                    res.json().then(user => {
+                        updateUser(user)
+                        navigate(`/users/${user.id}`)
+                    })
+                } else {
+                    res.json().then(json => setErrors(json.errors))
+                }
+            })
+    }
+    const changeHandler = (e) => {
+        const { name, value } = e.target
+        setFormData({ ...formData, [name]: value })
+    }
+    const clickHandler = (e) =>{
+        navigate('/signup')
+    }
+
+    return (
+        <div id='login-container'>
+            <h1>{location.pathname}</h1>
+            <form onSubmit={submitHandler}>
+                <label>
+                    Username
+                </label>
+                <input type='text' placeholder="Username" name='username' value={username} onChange={changeHandler} />
+                <br /> <br />
+                <label>
+                    Password
+                </label>
+            
+                <input type='password' placeholder="Password" name='password' value={password} onChange={changeHandler} />
+                <br />
+
+                <input type='submit' value='Log In' />
+            </form>
+            {errors ? <div>{errors}</div> : null}
+            <br />
+            <h3>or</h3>
+            <br />
+            <button onClick={clickHandler}>Signup with Randazon Burger</button>
+        </div>
+    )
+}
+
+export default LoginPage;
---
+++ client/src/components/SignupPage.js
@@ -0,0 +1,67 @@
+import { React, useState, useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+
+const SignupPage = ({ updateUser }) => {
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const [formData, setFormData] = useState({
+        first_name: '',
+        last_name:'',
+        username: '',
+        email: '',
+        password: '',
+        confirm_password: ''
+    })
+
+    const [errors, setErrors] = useState([])
+    const { first_name, last_name, username, email, password, confirm_password } = formData
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const newUser = {
+            name,
+            username,
+            email,
+            password,
+            confirm_password
+        }
+        if (password === confirm_password) {
+        fetch('/signup', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(newUser)
+        })
+            .then(res => {
+                if (res.ok) {
+                    res.json().then(user => {
+                        navigate(`/login`)
+                    })
+                } else {
+                    res.json().then(json => setErrors(Object.entries(json.errors)))
+                }
+            })
+        } else setErrors(['Passwords must match!'])
+    }
+    const changeHandler = (e) => {
+        const { name, value } = e.target
+        setFormData({ ...formData, [name]: value })
+    }
+
+    return (
+        <div>
+            <h1>{location.pathname}</h1>
+            <h2>Create your Randazon Burger Account</h2>
+            <form onSubmit={handleSubmit}>
+                <input placeholder="Name" type="text" name='name' value={name} onChange={changeHandler} />
+                <input placeholder="Username" type="text" name='username' value={username} onChange={changeHandler} />
+                <input placeholder="Email" type="text" name='email' value={email} onChange={changeHandler} />
+                <input placeholder="Password" type="text" name='password' value={password} onChange={changeHandler} />
+                <input placeholder="Confirm Password" type="text" name='confirm_password' value={confirm_password} onChange={changeHandler} />
+                <button type="submit">Signup</button>
+            </form>
+        </div>
+    )
+}
+
+export default SignupPage
